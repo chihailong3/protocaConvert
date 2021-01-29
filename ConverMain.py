3d8514a3ca3541e3cb0ae6f26cd31ddcf1da23c0
jkchihl@iwhr.com <jkchihl@iwhr.com>
2021-01-29 11:33:47 +0800

    配置文件

--- ConverMain.py
+++ ConverMain.py
@@ -12,26 +12,75 @@ Copyright © 2019, BITC, Co,. Ltd. All Rights Reserved.
 
 # ! /usr/bin/env python
 # coding=utf-8
+# //   作用： 将主站的串口报文转换成从站的网络接口的报文格式 从站以广播的形式接受串口
+# //            过来的数据 ；
+# //      port： 本地串口名称
+# //      slave_host_list ： 从站的IP地址和端口
 
 from socket import *
 
 import os
 import select ,logging
-logging.basicConfig(filename='DoorGuard.log',level=logging.INFO, format='%(asctime)s : %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
 logger = logging.getLogger(__name__)
 import serial
 import serial.tools.list_ports
+
+import  json ,sys
+# host_list = {
+#     "192.168.56.1": 8899,
+#     "192.169.11.106": 8877
+# }
+host_list = {}
+ip_list = []
+input  = []
+need_connect_list = []
+serial_name = ''
+is_running =True
+def read_config( ):
+    # 读取json文件内容,返回字典格式
+    logger.info (os.getcwd())
+    try:
+        with open('.//config.json','r',encoding='utf8')as fp:
+            json_data = json.load(fp)
+            global host_list,need_connect_list,ip_list,serial_name
+            serial_name = json_data['port']
+            slave_list = json_data['slave_host_list']
+            for slave in slave_list:
+                host_list[slave['ip']] = slave['port']
+            ip_list = [i for i in host_list.keys()]
+            need_connect_list = ip_list
+            print('Config json data :' )
+            print('____________________________')
+            print('{}'.format(json_data) )
+
+            print('____________________________')
+    except Exception as e:
+        global is_running
+        logger.error("{}".format(e))
+        is_running =False
+        sys.exit(-1)
 def mkpty():
     # 打开伪终端
-
+    global serial_name
     plist = list(serial.tools.list_ports.comports())
 
     if len(plist) <= 0:
         print("The Serial port can't find!")
     else:
         plist_0 = list(plist[0])
-        serialName = plist_0[0]
-        serialFd = serial.Serial(serialName, 9600, timeout=60)
+        for sname in plist_0:
+            if str(sname).upper() ==  serial_name.upper():
+                serial_name = serial_name.upper()
+                break
+        else:
+            logger.error('Port:{} does not exists in this machine!')
+            global is_running
+
+            is_running = False
+            sys.exit(-1)
+
+        serialFd = serial.Serial(serial_name, 9600, timeout=60)
         return serialFd
 
 import threading,time
@@ -44,11 +93,12 @@ class CThreadUartReceive(threading.Thread):
 
 
     def run(self):
-        while True:
+        global is_running
+        while is_running:
             if self.ser.isOpen() == False:
                 time.sleep(0.1)
                 continue
-            while True:
+            while is_running:
                 time.sleep(0.1)
                 n = self.ser.inWaiting()
                 if n == 0:
@@ -61,23 +111,7 @@ class CThreadUartReceive(threading.Thread):
                     except error:
                         pass
 
-import  json ,sys
 
-def read_config( ):
-    # 读取json文件内容,返回字典格式
-    logger.info (os.getcwd())
-    try:
-        with open('.//config.json','r',encoding='utf8')as fp:
-            json_data = json.load(fp)
-
-            print('Config json data :' )
-            print('____________________________')
-            print('{}'.format(json_data) )
-
-            print('____________________________')
-    except Exception as e:
-        logger.error("{}".format(e))
-        sys.exit(-1)
 
 def udp_broadcast_test():
 
@@ -93,13 +127,8 @@ def udp_broadcast_test():
         data, addr = s.recvfrom(1024)
         message = 'Received from %s:%s.' % (addr, data)
         print(message)
-host_list = {
-    "192.168.56.1": 8899,
-    "192.169.11.106": 8877
-}
-ip_list = [  i for i in host_list.keys() ]
-input  = []
-need_connect_list = (ip_list)
+
+
 
 def singleton(cls):
     # 单下划线的作用是这个变量只能在当前模块里访问,仅仅是一种提示作用
@@ -145,8 +174,8 @@ class CThreadSetUpConnection(threading.Thread):
         pass
 
     def run(self):
-        global need_connect_list
-        while True:
+        global need_connect_list ,is_running
+        while is_running:
             while need_connect_list:
                 self.doconnect()
                 time.sleep(0.2)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # protocaConvert
+
+#作用： 将主站的串口报文转换成从站的网络接口的报文格式 从站以广播的形式接受串口过来的数据 ；
+#port： 本地串口名称
+#slave_host_list ： 从站的IP地址和端口
+
 convert iec103 serial port to ethernet port 1 to N point converter
